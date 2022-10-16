9ee41bdf91ac55868d0e170261d1a82d46cc8a69
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2022-10-16 18:16:38 +0700

    Add files via upload

--- Project/script.js
+++ Project/script.js
@@ -10,12 +10,14 @@ let open_chest = document.getElementById('chest');
 let text = document.getElementById("intro");
 let bg = document.getElementById("bg");
 let note = document.getElementById("note");
+let click_circle = document.getElementById("click_circle");
 
 open_chest.addEventListener("click", function(item){
     document.getElementById("chest").src = "https://cdn.discordapp.com/attachments/817634371263397908/1030765156067708948/3.png";
     text.setAttribute("data-item-status", "open");
     bg.setAttribute("data-item-status", "open");
     note.setAttribute("data-item-status", "open");
+    click_circle.setAttribute("data-item-status", "open");
 });
 function openChest(item){
     console.log(item)
