5e6287c8a3aa296e89eb981c225a62a54e0e501e
it63070098 <74039435+it63070098@users.noreply.github.com>
2022-10-17 23:01:13 +0700

    Update script.js

--- nav/script.js
+++ nav/script.js
@@ -5,3 +5,25 @@ navBarToggle.addEventListener("click", function(){
     mainNav.classList.toggle('active');
 
 });
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-nav-toggle');
+
+navBarToggle.addEventListener("click", function(){
+    mainNav.classList.toggle('active');
+
+});
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
