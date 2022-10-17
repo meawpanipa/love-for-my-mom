869839b735495251514add0fa79436468f60244f
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2022-10-17 23:33:34 +0700

    Add files via upload

--- project_pu/script.js
+++ project_pu/script.js
@@ -12,6 +12,7 @@ let bg = document.getElementById("bg");
 let note = document.getElementById("note");
 let click_circle = document.getElementById("click_circle");
 let note_click = document.getElementById("note-open");
+let timeout;
 
 open_chest.addEventListener("click", function(item){
     document.getElementById("chest").src = "https://cdn.discordapp.com/attachments/817634371263397908/1030765156067708948/3.png";
@@ -27,8 +28,13 @@ function openChest(item){
 
 click_circle.addEventListener("click", function(item){
 
-    // note.setAttribute("data-item-status", "close");
-    // click_circle.setAttribute("data-item-status", "close");
-    // note_click.setAttribute("data-item-status", "open");
-    alert("text");
+    note.setAttribute("data-item-status", "close");
+    click_circle.setAttribute("data-item-status", "close");
+    note_click.setAttribute("data-item-status", "open");
+    timeout = setTimeout(alertFunc, 2000);
 });
+
+// function alertFunc() {
+
+//     alert("Hello!");
+//   }
