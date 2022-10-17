9f570edfb99b3b10ab2cc0919cfecb8de0190417
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2022-10-17 16:01:27 +0700

    Add files via upload

---
+++ project_pu/script.js
@@ -0,0 +1,34 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-nav-toggle');
+
+navBarToggle.addEventListener("click", function(){
+    mainNav.classList.toggle('active');
+
+});
+
+let open_chest = document.getElementById('chest');
+let text = document.getElementById("intro");
+let bg = document.getElementById("bg");
+let note = document.getElementById("note");
+let click_circle = document.getElementById("click_circle");
+let note_click = document.getElementById("note-open");
+
+open_chest.addEventListener("click", function(item){
+    document.getElementById("chest").src = "https://cdn.discordapp.com/attachments/817634371263397908/1030765156067708948/3.png";
+    text.setAttribute("data-item-status", "open");
+    bg.setAttribute("data-item-status", "open");
+    note.setAttribute("data-item-status", "open");
+    click_circle.setAttribute("data-item-status", "open");
+});
+function openChest(item){
+    console.log(item)
+    item.dataset.itemStatus = 'open';
+}
+
+click_circle.addEventListener("click", function(item){
+
+    // note.setAttribute("data-item-status", "close");
+    // click_circle.setAttribute("data-item-status", "close");
+    // note_click.setAttribute("data-item-status", "open");
+    alert("text");
+});
