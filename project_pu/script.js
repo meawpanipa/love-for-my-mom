569e6dcd25fe03c0a9754d4f6e278bc7df3c7dcd
IntanonTangtung <73689440+IntanonTangtung@users.noreply.github.com>
2022-10-31 22:13:01 +0700

    Add files via upload

--- project_pu/script.js
+++ project_pu/script.js
@@ -12,10 +12,11 @@ let bg = document.getElementById("bg");
 let note = document.getElementById("note");
 let click_circle = document.getElementById("click_circle");
 let note_click = document.getElementById("note-open");
+let note_aura = document.getElementById("note_aura");
 let timeout;
 
 open_chest.addEventListener("click", function(item){
-    document.getElementById("chest").src = "https://cdn.discordapp.com/attachments/817634371263397908/1030765156067708948/3.png";
+    document.getElementById("chest").src = "https://raw.githubusercontent.com/meawpanipa/love-for-my-mom/main/picture/%E0%B8%AB%E0%B8%B5%E0%B8%9A3-min.png";
     text.setAttribute("data-item-status", "open");
     bg.setAttribute("data-item-status", "open");
     note.setAttribute("data-item-status", "open");
@@ -31,10 +32,11 @@ click_circle.addEventListener("click", function(item){
     note.setAttribute("data-item-status", "close");
     click_circle.setAttribute("data-item-status", "close");
     note_click.setAttribute("data-item-status", "open");
-    timeout = setTimeout(alertFunc, 2000);
+    note_aura.setAttribute("data-item-status", "open");
+    // timeout = setTimeout(alertFunc, 2000);
 });
 
-// function alertFunc() {
+function changePage() {
 
-//     alert("Hello!");
-//   }
+    location.replace("page1.html")
+  }
